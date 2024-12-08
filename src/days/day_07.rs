65387dd66de823da6d08d23b242c8db4681edcbb
Arnaud Bouts <arnaud.bouts@gmail.com>
2024-12-08 15:27:23 +0000

    day 7  & 8

--- src/days/day_07.rs
+++ src/days/day_07.rs
@@ -13,23 +13,126 @@ impl D for Day {
 
     fn part_one(&self) -> i64 {
         
+        let mut res = 0;
+
         for line in &self.input {
-            todo!();
+            let line = line.split(": ").collect::<Vec<_>>();
+            let test = line[0].parse::<f64>().unwrap();
+            let numbers = line[1].split(" ").map(|v| v.parse::<f64>().unwrap()).collect::<Vec<_>>();
+
+            println!("");
+            if is_valid(test, numbers) {
+                res += test as i64;
+            }
         }
 
-        0
+        res
     }
 
     fn part_two(&self) -> i64 {    
-        todo!()
+
+        let mut res = 0;
+
+        for line in &self.input {
+            let line = line.split(": ").collect::<Vec<_>>();
+            let test = line[0].parse::<f64>().unwrap();
+            let numbers = line[1].split(" ").map(|v| v.parse::<f64>().unwrap()).collect::<Vec<_>>();
+
+            println!("");
+            if is_valid_2(test, numbers) {
+                res += test as i64;
+            }
+        }
+
+        res
     }
         
 }
 
-crate::test!(
-    0,
-    "",
+fn is_valid(test: f64, numbers: Vec<f64>) -> bool {
 
-    0,
-    ""
+    println!("test {test} {:?}", numbers);
+
+    if test == 0.0 && numbers.len() == 0 {
+        return true
+    }
+    if test == 0.0 || test.fract() != 0.0 || numbers.len() == 0 {
+        return false
+    }
+
+    println!("try -");
+    if is_valid(test - numbers[numbers.len()-1], numbers[0..numbers.len()-1].to_vec()) {
+        return true
+    }
+
+    println!("try /");
+    if is_valid(test / numbers[numbers.len()-1], numbers[0..numbers.len()-1].to_vec()) {
+        return true
+    }
+
+    false
+}
+
+fn is_valid_2(test: f64, numbers: Vec<f64>) -> bool {
+
+    println!("test {test} {:?}", numbers);
+
+    if test == 0.0 && numbers.len() == 0 {
+        return true
+    }
+    if test == 0.0 || test.fract() != 0.0 || numbers.len() == 0 {
+        return false
+    }
+
+    println!("try -");
+    if is_valid_2(test - numbers[numbers.len()-1], numbers[0..numbers.len()-1].to_vec()) {
+        return true
+    }
+
+    println!("try /");
+    if is_valid_2(test / numbers[numbers.len()-1], numbers[0..numbers.len()-1].to_vec()) {
+        return true
+    }
+
+    println!("try ||");
+    if test.to_string().ends_with(&numbers[numbers.len()-1].to_string()) {
+        let mut test = test.to_string();
+        test.truncate(test.to_string().len() - numbers[numbers.len()-1].to_string().len());
+        match test.parse::<f64>() {
+            Ok(test) => {
+                if is_valid_2(test, numbers[0..numbers.len()-1].to_vec()) {
+                    return true
+                }
+            }
+            Err(_) => {},
+        }
+    }
+
+    false
+}
+
+crate::test!(
+    3749,
+    "
+190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20",
+
+    11387,
+    "
+190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20"
 );
--- src/days/day_08.rs
+++ src/days/day_08.rs
@@ -1,4 +1,5 @@
 use crate::days::Day as D;
+use std::collections::HashMap;
 
 pub(crate) struct Day {
     input: Vec<String>
@@ -13,23 +14,169 @@ impl D for Day {
 
     fn part_one(&self) -> i64 {
         
+        let mut map: Vec<Vec<char>> = vec!();
+        let mut antinodes = vec!();
         for line in &self.input {
-            todo!();
+            map.push(line.chars().collect::<Vec<_>>());
+            antinodes.push(vec!['.'; line.len()]);
         }
 
-        0
+        let mut antennas = HashMap::new();
+        for i in 0..map.len() {
+            for j in 0..map[i].len() {
+                if map[i][j] != '.' {
+                    antennas.entry(map[i][j]).or_insert(vec!()).push(Point {i, j});
+                }
+            }
+        }
+
+        for frequency in antennas.keys() {
+
+            for a1 in antennas.get(frequency).unwrap() {
+                for a2 in antennas.get(frequency).unwrap() {
+                    if a1 == a2 {
+                        continue
+                    }
+
+                    match a1.resonnate(a2, (map.len(), map[0].len())) {
+                        Some(r) => antinodes[r.i][r.j] = '#',
+                        _ => {}
+                    }
+
+                    match a2.resonnate(a1, (map.len(), map[0].len())) {
+                        Some(r) => antinodes[r.i][r.j] = '#',
+                        _ => {}
+                    }
+                }
+            }
+
+        }
+
+
+        println!();
+        for i in 0..map.len() {
+            println!("{} {}", antinodes[i].iter().collect::<String>(), map[i].iter().collect::<String>());
+        }    
+
+        antinodes.iter().map(|l| l.iter().filter(|c| c == &&'#').count() as i64).sum()
     }
 
     fn part_two(&self) -> i64 {    
-        todo!()
+        let mut map: Vec<Vec<char>> = vec!();
+        let mut antinodes = vec!();
+        for line in &self.input {
+            map.push(line.chars().collect::<Vec<_>>());
+            antinodes.push(vec!['.'; line.len()]);
+        }
+
+        let mut antennas = HashMap::new();
+        for i in 0..map.len() {
+            for j in 0..map[i].len() {
+                if map[i][j] != '.' {
+                    antennas.entry(map[i][j]).or_insert(vec!()).push(Point {i, j});
+                }
+            }
+        }
+
+        for frequency in antennas.keys() {
+
+            for a1 in antennas.get(frequency).unwrap() {
+                for a2 in antennas.get(frequency).unwrap() {
+                    if a1 == a2 {
+                        continue
+                    }
+
+                    for r in a1.resonnate_all(a2, (map.len(), map[0].len())) {
+                        antinodes[r.i][r.j] = '#'
+                    }
+
+                    for r in a2.resonnate_all(a1, (map.len(), map[0].len())) {
+                        antinodes[r.i][r.j] = '#'
+                    }
+                }
+            }
+
+        }
+
+
+        println!();
+        for i in 0..map.len() {
+            println!("{} {}", antinodes[i].iter().collect::<String>(), map[i].iter().collect::<String>());
+        }    
+
+        antinodes.iter().map(|l| l.iter().filter(|c| c == &&'#').count() as i64).sum()
     }
-        
+
+}
+
+#[derive(PartialEq)]
+struct Point {
+    i: usize,
+    j: usize,
+}
+
+impl Point {
+    fn resonnate(&self, other: &Point, (height, width): (usize, usize)) -> Option<Self> {
+
+        let di = other.i as i64 - self.i as i64;
+        let dj = other.j as i64 - self.j as i64;
+
+        if other.i as i64 + di < 0 || other.i as i64 + di >= height as i64
+        || other.j as i64 + dj < 0 || other.j as i64 + dj >= width as i64 {
+            None
+        } else {
+            Some(Point {i: (other.i as i64 + di) as usize, j: (other.j as i64 + dj) as usize})
+        }
+    }
+
+    fn resonnate_all(&self, other: &Point, (height, width): (usize, usize)) -> Vec<Self> {
+
+        let di = other.i as i64 - self.i as i64;
+        let dj = other.j as i64 - self.j as i64;
+
+        let mut res = vec!();
+        let mut new_i = other.i as i64;
+        let mut new_j = other.j as i64;
+        while new_i >= 0 && new_i < height as i64
+        && new_j >= 0 && new_j < width as i64 {
+            res.push(Point{i: new_i as usize, j: new_j as usize});
+            new_i += di;
+            new_j += dj;
+        }
+
+        res
+    }
+
 }
 
 crate::test!(
-    0,
-    "",
+    14,
+    "
+............
+........0...
+.....0......
+.......0....
+....0.......
+......A.....
+............
+............
+........A...
+.........A..
+............
+............",
 
-    0,
-    ""
+    34,
+    "
+............
+........0...
+.....0......
+.......0....
+....0.......
+......A.....
+............
+............
+........A...
+.........A..
+............
+............"
 );
