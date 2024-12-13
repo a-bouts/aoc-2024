37edec0dd1ceaa2a6910c72d039f08c7cf6ea4ee
Arnaud Bouts <arnaud.bouts@gmail.com>
2024-12-13 08:16:42 +0000

    day 12 & 13

--- src/days/day_12.rs
+++ src/days/day_12.rs
@@ -12,24 +12,318 @@ impl D for Day {
     }
 
     fn part_one(&self) -> i64 {
-        
-        for line in &self.input {
-            todo!();
+
+        fn regionize(map: &mut Vec<Vec<char>>, (i, j): (usize, usize)) {
+            let change = &mut vec![(i, j)];
+            let c = map[i][j];
+            map[i][j] = '#';
+
+            println!("regionize {c}, ({i}, {j})");
+
+            while change.len() > 0 {
+                let mut new_change = vec![];
+
+                for (i, j) in change.iter() {
+                    let (i, j) = (*i, *j);
+                    if i > 0 && map[i-1][j] == c {
+                        map[i-1][j] = '#';
+                        new_change.push((i-1, j));
+                    }
+                    if j > 0 && map[i][j-1] == c {
+                        map[i][j-1] = '#';
+                        new_change.push((i, j-1));
+                    }
+                    if i < map.len()-1 && map[i+1][j] == c {
+                        map[i+1][j] = '#';
+                        new_change.push((i+1, j));
+                    }
+                    if j < map[i].len()-1 && map[i][j+1] == c {
+                        map[i][j+1] = '#';
+                        new_change.push((i, j+1));
+                    }
+                }
+
+                *change = new_change;
+            }
         }
 
-        0
+        fn area(map: &Vec<Vec<char>>) -> i64 {
+            let mut res = 0;
+            for i in 0..map.len() {
+                for j in 0..map[i].len() {
+                    if map[i][j] == '#' {
+                        res += 1;
+                    }
+                }
+            }
+            res
+        }
+
+        fn perimeter(map: &Vec<Vec<char>>) -> i64 {
+            let mut res = 0;
+
+            for i in 0..map.len() {
+                let mut is_plot = false;
+                let mut nb_switch = 0;
+                for j in 0..map[i].len() {
+                    if !is_plot && map[i][j] == '#'
+                    || is_plot && map[i][j] != '#' {
+                        nb_switch += 1;
+                        is_plot = !is_plot;
+                    }
+                    if j == map[i].len() - 1 && map[i][j] == '#' {
+                        nb_switch += 1;
+                    }
+                }
+                res += nb_switch;
+            }
+
+            for j in 0..map[0].len() {
+                let mut is_plot = false;
+                let mut nb_switch = 0;
+                for i in 0..map.len() {
+                    if !is_plot && map[i][j] == '#'
+                    || is_plot && map[i][j] != '#' {
+                        nb_switch += 1;
+                        is_plot = !is_plot;
+                    }
+                    if i == map.len() - 1 && map[i][j] == '#' {
+                        nb_switch += 1;
+                    }
+                }
+                res += nb_switch;
+            }
+            res
+        }
+
+        fn clean(map: &mut Vec<Vec<char>>) {
+            for i in 0..map.len() {
+                for j in 0..map[i].len() {
+                    if map[i][j] == '#' {
+                        map[i][j] = '.';
+                    }
+                }
+            }
+        }
+        
+        let mut map = vec!();
+        for line in &self.input {
+            map.push(line.chars().collect::<Vec<_>>());
+        }
+
+        let mut res = 0;
+
+        let mut nb_plots = (map.len() * map[0].len()) as i64;
+
+        let map = &mut map;
+        while nb_plots > 0 {
+            for i in 0..map.len() {
+                for j in 0..map[i].len() {
+                    if map[i][j] != '.' {
+                        regionize(map, (i,j));
+
+                        let a = area(&map);
+                        let p = perimeter(&map);
+
+                        res += a * p;
+                        nb_plots -= a;
+
+                        println!("{a} * {p}");
+                        //for l in map.iter() {
+                        //    println!("{}", l.iter().collect::<String>());
+                        //}
+
+                        clean(map);
+                        
+                    }
+                }
+            }
+        }
+
+        res
     }
 
     fn part_two(&self) -> i64 {    
-        todo!()
+
+        fn regionize(map: &mut Vec<Vec<char>>, (i, j): (usize, usize)) {
+            let change = &mut vec![(i, j)];
+            let c = map[i][j];
+            map[i][j] = '#';
+
+            println!("regionize {c}, ({i}, {j})");
+
+            while change.len() > 0 {
+                let mut new_change = vec![];
+
+                for (i, j) in change.iter() {
+                    let (i, j) = (*i, *j);
+                    if i > 0 && map[i-1][j] == c {
+                        map[i-1][j] = '#';
+                        new_change.push((i-1, j));
+                    }
+                    if j > 0 && map[i][j-1] == c {
+                        map[i][j-1] = '#';
+                        new_change.push((i, j-1));
+                    }
+                    if i < map.len()-1 && map[i+1][j] == c {
+                        map[i+1][j] = '#';
+                        new_change.push((i+1, j));
+                    }
+                    if j < map[i].len()-1 && map[i][j+1] == c {
+                        map[i][j+1] = '#';
+                        new_change.push((i, j+1));
+                    }
+                }
+
+                *change = new_change;
+            }
+        }
+
+        fn area(map: &Vec<Vec<char>>) -> i64 {
+            let mut res = 0;
+            for i in 0..map.len() {
+                for j in 0..map[i].len() {
+                    if map[i][j] == '#' {
+                        res += 1;
+                    }
+                }
+            }
+            res
+        }
+
+        fn perimeter(map: &Vec<Vec<char>>) -> i64 {
+            let mut res = 0;
+
+            for i in 0..map.len() {
+                let mut is_plot = false;
+                let mut nb_switch = 0;
+                for j in 0..map[i].len() {
+                    if !is_plot && map[i][j] == '#' {
+                        if i == 0 || map[i-1][j] != '#' || j > 0 && map[i-1][j-1] == '#' {
+                            println!("1 - border : ({i}, {j}) : {is_plot}");
+                            nb_switch += 1;
+                        }
+                        is_plot = !is_plot;
+                    } else if is_plot && map[i][j] != '#' {
+                        if i == 0 || map[i-1][j] == '#' || j > 0 && map[i-1][j-1] != '#' {
+                            println!("2 - border : ({i}, {j}) : {is_plot}");
+                            nb_switch += 1;
+                        }
+                        is_plot = !is_plot;
+                    }
+                    if j == map[i].len() - 1 && map[i][j] == '#' {
+                        if i == 0 || map[i-1][j] != '#' {
+                            println!("3 - border : ({i}, {j}) : {is_plot}");
+                            nb_switch += 1;
+                        }
+                    }
+                }
+                res += nb_switch;
+            }
+
+            for j in 0..map[0].len() {
+                let mut is_plot = false;
+                let mut nb_switch = 0;
+                for i in 0..map.len() {
+                    if !is_plot && map[i][j] == '#' {
+                        if j == 0 || map[i][j-1] != '#' || i > 0 && map[i-1][j-1] == '#' {
+                            println!("4 - border : ({i}, {j}) : {is_plot}");
+                            nb_switch += 1;
+                        }
+                        is_plot = !is_plot;
+                    } else if is_plot && map[i][j] != '#' {
+                        if j == 0 || map[i][j-1] == '#' || i > 0 && map[i-1][j-1] != '#' {
+                            println!("5 - border : ({i}, {j}) : {is_plot}");
+                            nb_switch += 1;
+                        }
+                        is_plot = !is_plot;
+                    }
+                    if i == map.len() - 1 && map[i][j] == '#' {
+                        if j == 0 || map[i][j-1] != '#' {
+                            println!("6 - border : ({i}, {j}) : {is_plot}");
+                            nb_switch += 1;
+                        }
+                    }
+                }
+                res += nb_switch;
+            }
+            res
+        }
+
+        fn clean(map: &mut Vec<Vec<char>>) {
+            for i in 0..map.len() {
+                for j in 0..map[i].len() {
+                    if map[i][j] == '#' {
+                        map[i][j] = '.';
+                    }
+                }
+            }
+        }
+        
+        let mut map = vec!();
+        for line in &self.input {
+            map.push(line.chars().collect::<Vec<_>>());
+        }
+
+        let mut res = 0;
+
+        let mut nb_plots = (map.len() * map[0].len()) as i64;
+
+        let map = &mut map;
+        while nb_plots > 0 {
+            for i in 0..map.len() {
+                for j in 0..map[i].len() {
+                    if map[i][j] != '.' {
+                        regionize(map, (i,j));
+
+                        let a = area(&map);
+                        let p = perimeter(&map);
+
+                        res += a * p;
+                        nb_plots -= a;
+
+                        println!("{a} * {p}");
+                        //for l in map.iter() {
+                        //   println!("{}", l.iter().collect::<String>());
+                        //}
+
+                        clean(map);
+                        
+                    }
+                }
+            }
+        }
+
+        res
+
     }
         
 }
 
 crate::test!(
-    0,
-    "",
+    1930,
+    "
+RRRRIICCFF
+RRRRIICCCF
+VVRRRCCFFF
+VVRCCCJFFF
+VVVVCJJCFE
+VVIVCCJJEE
+VVIIICJJEE
+MIIIIIJJEE
+MIIISIJEEE
+MMMISSJEEE",
 
-    0,
-    ""
+    1206,
+    "
+RRRRIICCFF
+RRRRIICCCF
+VVRRRCCFFF
+VVRCCCJFFF
+VVVVCJJCFE
+VVIVCCJJEE
+VVIIICJJEE
+MIIIIIJJEE
+MIIISIJEEE
+MMMISSJEEE"
 );
--- src/days/day_13.rs
+++ src/days/day_13.rs
@@ -1,3 +1,5 @@
+use regex::Regex;
+
 use crate::days::Day as D;
 
 pub(crate) struct Day {
@@ -12,23 +14,93 @@ impl D for Day {
     }
 
     fn part_one(&self) -> i64 {
-        
-        for line in &self.input {
-            todo!();
-        }
+        let mut res: i64 = 0;
 
-        0
+        let mut l = 0;
+        while l < self.input.len() {
+            let a = &self.input[l];
+            let b = &self.input[l+1];
+            let p = &self.input[l+2];
+            
+            let re = Regex::new(r"Button [AB]: X\+(?<x>[0-9]*), Y\+(?<y>[0-9]*)").unwrap();
+            let caps = re.captures(a).unwrap();
+            let a = (caps["x"].parse::<i64>().unwrap(), caps["y"].parse::<i64>().unwrap());
+            let caps = re.captures(b).unwrap();
+            let b = (caps["x"].parse::<i64>().unwrap(), caps["y"].parse::<i64>().unwrap());
+            let re = Regex::new(r"Prize: X=(?<x>[0-9]*), Y=(?<y>[0-9]*)").unwrap();
+            let caps = re.captures(p).unwrap();
+            let p = (caps["x"].parse::<i64>().unwrap(), caps["y"].parse::<i64>().unwrap());
+
+            l += 4;
+
+            //u*a.0 + v*b.0 = p.0 (*a.1)
+            //u*a.1 + v*b.1 = p.1 (*a.0)
+
+            let v = (p.1 * a.0 - p.0 * a.1) / (b.1 * a.0 - b.0 * a.1);
+            let u = (p.1 * b.0 - p.0 * b.1) / (a.1 * b.0 - a.0 * b.1);
+
+            if u*a.0+v*b.0 == p.0 && u*a.1+v*b.1 == p.1 {
+                res += 3 * u + v;
+            }
+        }
+       
+        res
     }
 
     fn part_two(&self) -> i64 {    
-        todo!()
+        let mut res: i64 = 0;
+
+        let mut l = 0;
+        while l < self.input.len() {
+            let a = &self.input[l];
+            let b = &self.input[l+1];
+            let p = &self.input[l+2];
+            
+            let re = Regex::new(r"Button [AB]: X\+(?<x>[0-9]*), Y\+(?<y>[0-9]*)").unwrap();
+            let caps = re.captures(a).unwrap();
+            let a = (caps["x"].parse::<i64>().unwrap(), caps["y"].parse::<i64>().unwrap());
+            let caps = re.captures(b).unwrap();
+            let b = (caps["x"].parse::<i64>().unwrap(), caps["y"].parse::<i64>().unwrap());
+            let re = Regex::new(r"Prize: X=(?<x>[0-9]*), Y=(?<y>[0-9]*)").unwrap();
+            let caps = re.captures(p).unwrap();
+            let p = (caps["x"].parse::<i64>().unwrap() + 10000000000000, caps["y"].parse::<i64>().unwrap() + 10000000000000);
+
+            l += 4;
+
+            //u*a.0 + v*b.0 = p.0 (*a.1)
+            //u*a.1 + v*b.1 = p.1 (*a.0)
+
+            let v = (p.1 * a.0 - p.0 * a.1) / (b.1 * a.0 - b.0 * a.1);
+            let u = (p.1 * b.0 - p.0 * b.1) / (a.1 * b.0 - a.0 * b.1);
+
+            if u*a.0+v*b.0 == p.0 && u*a.1+v*b.1 == p.1 {
+                res += 3 * u + v;
+            }
+        }
+       
+        res
     }
         
 }
 
 crate::test!(
-    0,
-    "",
+    480,
+    "
+Button A: X+94, Y+34
+Button B: X+22, Y+67
+Prize: X=8400, Y=5400
+
+Button A: X+26, Y+66
+Button B: X+67, Y+21
+Prize: X=12748, Y=12176
+
+Button A: X+17, Y+86
+Button B: X+84, Y+37
+Prize: X=7870, Y=6450
+
+Button A: X+69, Y+23
+Button B: X+27, Y+71
+Prize: X=18641, Y=10279",
 
     0,
     ""
